73579bede06054579fde7bd262de61c9542d68e6
shanmukha <ayyappareddy2005@gmail.com>
2026-03-17 08:48:42 +0000

    Updated in main branch

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
-// Calculator.java
+// Calculator.java (main branch)
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello from Main Branch");
+        System.out.println("Hello from Main Branch - Updated");
     }
 }
